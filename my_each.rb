a847dd1194e259e2fe4e6188a6ebfc3229f6b82d
Cedric David <cedric.david@btopenworld.com>
2016-12-05 03:44:30 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+
+  c = 0
+  while c < array.size
+    i = array[c]
+    yield
+    c += 1
+  end
+
 end
